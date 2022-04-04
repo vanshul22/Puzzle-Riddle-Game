1ffdfdc21b846bc5fbbc16025f503b634dc11f10
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-04 23:54:09 +0530

    Version : 1.1.0 : Initial Commit.

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+/*
+Website Name - Puzzle-Riddle-Game
+Name : Vanshul Kesharwani
+Date : 04/04/2022
+Version : 1.1.1
+Email : vkvanshulkesharwani54@gmail.com
+Description : This is a Web app for playing puzzle game. We can play game of puzzle. Works without reloading page with JavaScript.
+*/
---
+++ README.md
@@ -0,0 +1,4 @@
+# Puzzle-Riddle-Game
+This is a Web app for playing puzzle game. Works without reloading page with JavaScript.
+
+To check live page please [Click Here](https://vanshul22.github.io/Puzzle-Riddle-Game/).
