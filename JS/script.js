4def378dd1ba72505a5af4a9f6ef75670dcfa20d
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-05 00:53:49 +0530

    Version 2.1.1 : Added 9 boxex with their respective classes, Added CSS in all required boxes.

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 Website Name - Puzzle-Riddle-Game
 Name : Vanshul Kesharwani
 Date : 04/04/2022
-Version : 1.1.1
+Version : 2.1.1
 Email : vkvanshulkesharwani54@gmail.com
 Description : This is a Web app for playing puzzle game. We can play game of puzzle. Works without reloading page with JavaScript.
 */
